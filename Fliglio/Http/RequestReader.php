dc5ef17268246189321c668311ba28d7ad377a86
Ben Schwartz <benschw@gmail.com>
2015-02-25 09:58:19 -0600

    feat(RequestReader): adding RequestReader interface

---
+++ Fliglio/Http/RequestReader.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Fliglio\Http;
+
+interface RequestReader {
+	
+	public function getHttpMethod();
+	public function getProtocol();
+	public function getHost();
+	public function getUrl();
+
+	public function getGetParams();
+	public function getPostData();
+	
+	public function addHeader($key, $val);
+	public function getHeaders();
+	public function isHeaderSet($key);
+	public function getHeader($key);
+
+}
