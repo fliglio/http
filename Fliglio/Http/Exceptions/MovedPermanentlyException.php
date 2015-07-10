fdc3fef75b24001b66ff1aa45a371ff4e100c3d5
Philip W <philip.whitt@sbcglobal.net>
2015-07-10 16:00:06 -0500

    Adding LocationStatusException to moved exception

--- Fliglio/Http/Exceptions/MovedPermanentlyException.php
+++ Fliglio/Http/Exceptions/MovedPermanentlyException.php
@@ -4,15 +4,8 @@ namespace Fliglio\Http\Exceptions;
 
 use Fliglio\Http\Http;
 
-class MovedPermanentlyException extends HttpStatusException {
+class MovedPermanentlyException extends LocationStatusException {
 
 	protected static $status = Http::STATUS_MOVED_PERMANENTLY;
 
-	private $location;
-	public function __construct($location = '') {
-		$this->location = $location;
-	}
-	public function getLocation() {
-		return $this->location;
-	}
 }
