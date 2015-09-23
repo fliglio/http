77323675adf7e9214bfc688d37ea814dcdb974e2
Philip W <philip.whitt@sbcglobal.net>
2015-09-23 11:42:30 -0500

    Adding optional error info property to HttpStatusException

--- Fliglio/Http/Exceptions/HttpStatusException.php
+++ Fliglio/Http/Exceptions/HttpStatusException.php
@@ -6,8 +6,17 @@ class HttpStatusException extends \Exception {
 
 	protected static $status;
 
+	private $errorInfo;
+
 	public function getStatusCode() {
 		return static::$status;
+	}
+	public function getErrorInfo() {
+		return $this->errorInfo;
+	}
+	public function setErrorInfo($errorInfo) {
+		$this->errorInfo = $errorInfo;
+		return $this;
 	}
 
 }
